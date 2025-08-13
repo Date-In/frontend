80ea86cba6f3d4ae119b296ed66f564d9a65d1f9
fevoritos <fevoritos@mail.ru>
2025-08-13 23:14:49 +0300

    init next.js

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <div className="text-center mt-16 text-4xl font-bold text-red">
+        Date In
+      </div>
+    </div>
+  );
+}
